325f8440d649354856b2d02ee62cf2039525ac0d
Natanael Silva <natan.jhs6991@gmail.com>
2021-02-17 23:39:46 -0300

    Criation of logic the operations

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
-import './Button.css'
+import React from "react";
+import "./Button.css";
 
-export default props => {
-    <button className="">0</button>
-}
+export default (props) => {
+  // let classes = 'button'
+  // classes += props.opration ? 'operation' : ""
+  // classes += props.bouble ? 'double' : ""
+  // classes += props.triple ? 'triple' : ""
+
+  return (
+    <button
+      className={`
+            button
+            ${props.operation ? "operation" : ""}
+            ${props.double ? "double" : ""}
+            ${props.triple ? "triple" : ""}
+        
+        `}
+        onClick={event => props.click && props.click(props.label) }
+    >
+      {props.label}
+    </button>
+  );
+};
