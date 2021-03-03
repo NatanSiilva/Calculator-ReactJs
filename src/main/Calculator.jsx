c3ac550654f16b37ee0889dc0fddff9404026363
Natanael Silva <natan.jhs6991@gmail.com>
2021-03-02 21:06:47 -0300

    Finalization of project calculator

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -28,19 +28,33 @@ class Calculator extends Component {
   }
 
   setOperation(operation) {
-    if(this.state.current === 0) {
-      this.setState({ operation, current: 1, clearDisplay: true })
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
     } else {
-      const equals = oeration === '='
-      const currentOperation = this.state.operation
-      
+      const equals = operation === "=";
+      const currentOperation = this.state.operation;
+
+      const values = [...this.state.values];
+      try {
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+      } catch (error) {
+        values[0] = this.state.values[0];
+      }
+
+      values[1] = 0;
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      });
     }
   }
 
   addDigit(digit) {
-    if (digit === "." && this.state.displayValue.includes(".")) {
-      return;
-    }
+    if (digit === "." && this.state.displayValue.includes(".")) return;
     const clearDisplay =
       this.state.displayValue === "0" || this.state.clearDisplay;
     const currentValue = clearDisplay ? "" : this.state.displayValue;
@@ -53,7 +67,7 @@ class Calculator extends Component {
       const values = [...this.state.values];
       values[i] = newValues;
       this.setState({ values });
-      console.log(values)
+      console.log(values);
     }
   }
 
